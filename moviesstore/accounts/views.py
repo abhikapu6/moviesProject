010cee742b964b9eb95e94f5b890ead20e21b2dc
abhikapu6 <164439997+abhikapu6@users.noreply.github.com>
2026-01-26 13:24:52 -0500

    Adding changes for chapter 11 and 12

--- moviesstore/accounts/views.py
+++ moviesstore/accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 from django.shortcuts import redirect
 from .forms import CustomUserCreationForm, CustomErrorList
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 from django.contrib.auth.forms import UserCreationForm
@@ -47,3 +49,11 @@ def signup(request):
             template_data['form'] = form
             return render(request, 'accounts/signup.html',
                 {'template_data': template_data})
+        
+@login_required
+def orders(request):
+    template_data = {}
+    template_data['title'] = 'Orders'
+    template_data['orders'] = request.user.order_set.all()
+    return render(request, 'accounts/orders.html',
+        {'template_data': template_data})
